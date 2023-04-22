767c99d9c1ccaa07a92febfd41dbca51cbc7eaa3
Harsh Sangani <harshsangani001@gmail.com>
2023-04-22 16:54:31 +0530

    ui 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_practtise/practise/ui1/ui1_page.dart';
+import 'package:my_practtise/practise/ui2/ui2_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Ui1(),
+      home: Ui2(),
     );
   }
 }
---
+++ lib/practise/ui2/ui2_page.dart
@@ -0,0 +1,577 @@
+import 'package:flutter/material.dart';
+
+class Ui2 extends StatelessWidget {
+  const Ui2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[50],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Icon(
+                Icons.menu,
+                color: Colors.deepPurple,
+                size: 30,
+              ),
+              Icon(
+                Icons.account_circle,
+                color: Colors.deepPurple,
+                size: 30,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Hii ",
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    "Foad",
+                    style: TextStyle(
+                      color: Colors.deepPurple[400],
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Tehran 18°",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 43,
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12,
+            ),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Search or Type Web Address",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Icon(
+                  Icons.mic,
+                  color: Colors.deepPurple,
+                  size: 30,
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Dribbble",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Apple",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Linkdin",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Spotify",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Figma",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Trello",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Twitter",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.shade200,
+                          blurRadius: 10,
+                          offset: const Offset(2, 3),
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.sports_baseball_outlined,
+                      color: Colors.deepPurple[700],
+                      size: 50,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    "Instagram",
+                    style: TextStyle(
+                      color: Colors.deepPurple[200],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 43,
+              ),
+              Text(
+                "Today News",
+                style: TextStyle(
+                  color: Colors.deepPurple[400],
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 150,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 43,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.deepPurple[100],
+            ),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 20,
+                ),
+                Image.network(
+                  "https://apksos.com/storage/images/wallpaper/ios/iphone/wallpaper.ios.iphone_1.png",
+                  width: 130,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "9 to 5 Mac",
+                    ),
+                    const Text(
+                      "Everthing New in ios 15:",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const Text(
+                      "App Privacy Report and",
+                    ),
+                    const Text(
+                      "Auto Call Updates",
+                    ),
+                    SizedBox(
+                      // color: Colors.red,
+                      height: 30,
+                      width: 230,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: const [
+                                Icon(Icons.favorite_outline),
+                                Icon(Icons.bookmark_border),
+                              ],
+                            ),
+                            const Text("1 day ago"),
+                          ]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.deepPurple.shade200,
+                      blurRadius: 10,
+                      offset: const Offset(2, 3),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.home_outlined,
+                  color: Colors.deepPurple[700],
+                  size: 50,
+                ),
+              ),
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.deepPurple.shade200,
+                      blurRadius: 10,
+                      offset: const Offset(2, 3),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.search,
+                  color: Colors.deepPurple[700],
+                  size: 50,
+                ),
+              ),
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.deepPurple.shade200,
+                      blurRadius: 10,
+                      offset: const Offset(2, 3),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.add_box,
+                  color: Colors.deepPurple[700],
+                  size: 50,
+                ),
+              ),
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.deepPurple.shade200,
+                      blurRadius: 10,
+                      offset: const Offset(2, 3),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.bookmark_border,
+                  color: Colors.deepPurple[700],
+                  size: 50,
+                ),
+              ),
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.deepPurple.shade200,
+                      blurRadius: 10,
+                      offset: const Offset(2, 3),
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.more_horiz_outlined,
+                  color: Colors.deepPurple[700],
+                  size: 50,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
