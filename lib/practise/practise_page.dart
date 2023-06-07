c2f8b4af4e5c7b3e496446eb54aeef26d8b9c8c3
Harsh Sangani <harshsangani001@gmail.com>
2023-06-07 12:01:04 +0530

    drawer

--- lib/practise/practise_page.dart
+++ lib/practise/practise_page.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 
 class PractisePage extends StatelessWidget {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   PractisePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () => scaffoldKey.currentState?.openDrawer(),
-            icon: const Icon(Icons.search)),
-      ),
       drawer: const Drawer(),
+      // key: scaffoldKey,
+      body: Builder(builder: (context) {
+        return IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+              // scaffoldKey.currentState!.openEndDrawer();
+              // setState(() {});
+            },
+            icon: const Icon(Icons.search));
+      }),
     );
   }
 }
