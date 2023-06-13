344648eadfe33154ce2d36c351e05da021ecfeab
Harsh Sangani <harshsangani001@gmail.com>
2023-06-13 15:36:20 +0530

    cross color

--- lib/stateful_example/cross_color_app/cross_color_controller.dart
+++ lib/stateful_example/cross_color_app/cross_color_controller.dart
@@ -1,19 +1,38 @@
-import 'package:flutter/material.dart';
+// List<Color> colors = [
+//   Colors.green,
+//   Colors.red,
+// ];
 
-List<Color> colors = [
-  Colors.green,
-  Colors.red,
-];
+// List<int> number = [];
+// int count = 0;
 
 class CrossColor {
-  static Color colorChangeData(int index) {
-    if (index % 2 == 0) {
-      return colors[0];
-    } else {
-      Color temp = colors[0];
-      colors[0] = colors[1];
-      colors[1] = temp;
-      return colors[0];
+//   static Color colorChangeData(int index) {
+//     if (index % 2 == 0) {
+//       return colors[0];
+//     } else {
+//       Color temp = colors[0];
+//       colors[0] = colors[1];
+//       colors[1] = temp;
+//       return colors[0];
+//     }
+//   }
+
+  // static Color colorChangeIndex(int index) {
+  //   if (index == number[count]) {
+  //     count++;
+  //     return Colors.red;
+  //   } else {
+  //     return Colors.green;
+  //   }
+  // }
+  static List inputedData = List.generate(30, (index) => index);
+  static List outputData = [];
+  static void crossPattern() {
+    for (var i in inputedData) {
+      if (i % 4 == 0 || (i - 3) % 4 == 0) {
+        outputData.add(i);
+      }
     }
   }
 }
--- lib/stateful_example/cross_color_app/cross_color_ui.dart
+++ lib/stateful_example/cross_color_app/cross_color_ui.dart
@@ -11,6 +11,21 @@ class CrossColorDemo extends StatefulWidget {
 class _CrossColorDemoState extends State<CrossColorDemo> {
   @override
   Widget build(BuildContext context) {
+    CrossColor.crossPattern();
+    // int index = 30;
+    // List<int> number = [];
+    // int i = 0;
+    // do {
+    //   if (i % 2 == 0) {
+    //     number.add(i);
+    //     i += 3;
+    //   } else {
+    //     number.add(i);
+    //     i += 1;
+    //   }
+    // } while (i < index);
+    // number.add(0);
+
     return Scaffold(
       body: GridView.count(
         crossAxisCount: 2,
@@ -18,10 +33,12 @@ class _CrossColorDemoState extends State<CrossColorDemo> {
         mainAxisSpacing: 5,
         children: [
           ...List.generate(
-            30,
+            CrossColor.inputedData.length,
             (index) => Container(
               decoration: BoxDecoration(
-                color: CrossColor.colorChangeData(index),
+                color: CrossColor.outputData.contains(index)
+                    ? Colors.red
+                    : Colors.green,
                 borderRadius: BorderRadius.circular(20),
               ),
               alignment: Alignment.center,
